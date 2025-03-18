dc3d5c6b1b1c3be351fdee4b1f28a3ef632c32bb
Gloria Zhu <gloriazhu00@gmail.com>
2025-03-18 11:45:46 -0400

    remove buckets

--- kenknowlton/sketch.js
+++ kenknowlton/sketch.js
@@ -5,7 +5,7 @@ var h = 390;
 var chunk_size = 6;
 var icon_map = []
 
-var buckets = 14 // set to -1 to disable buckets (have 256 unique icons)
+var buckets = -1 // set to -1 to disable buckets (have 256 unique icons)
 
 // load icons here
 function preload(){
